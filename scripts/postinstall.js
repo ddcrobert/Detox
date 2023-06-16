945bb02e05f55e50ca5bbb87581a12d97ece5f46
ddcrobert <ddc.robert@gmail.com>
2023-06-16 09:04:09 +0200

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,8 +3,8 @@
 const cp = require('child_process');
 const path = require('path');
 
-if (process.platform === 'darwin' && !process.env.DETOX_DISABLE_POD_INSTALL) {
-  cp.execSync('bundle exec pod install', { 
+if (false && process.platform === 'darwin' && !process.env.DETOX_DISABLE_POD_INSTALL) {
+  cp.execSync('pod install', { 
     cwd: path.join(process.cwd(), 'detox/test/ios'),
     stdio: 'inherit'
   });
